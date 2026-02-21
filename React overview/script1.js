9b2fca88424521a7c1fe25588a41f90ef930d75b
Vishvajit <vishvajit6264@gmail.com>
2026-02-21 11:20:39 +0530

    added

---
+++ React overview/script1.js
@@ -0,0 +1,7 @@
+//Chapter 1: Introduction to JavaScript
+console.log("Hello, World! This is script1.js");
+
+//Chapter 2: Variables and Data Types
+var x = 10;
+var x = 20;
+console.log(x); // Output: 20
